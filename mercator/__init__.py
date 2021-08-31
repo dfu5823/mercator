27cdf63ea7f42fef10612b128974b655b2b3808b
Me <maldonadojsm@gmail.com>
2021-08-31 21:30:24 +0200

    still experimenting

--- mercator/__init__.py
+++ mercator/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '0.1.0'
+
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+
+@app.route('/health')
+def check_health():
+    return "OK"

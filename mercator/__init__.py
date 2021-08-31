ec6c31673d5f6ee12c3d62dc39486cf54f341794
Me <maldonadojsm@gmail.com>
2021-08-31 21:50:36 +0200

    Migrating app into __init__.py

--- mercator/__init__.py
+++ mercator/__init__.py
@@ -1,16 +1,26 @@
 __version__ = '0.1.0'
 
-from flask import Flask
+from dataclasses import dataclass
+import flask
+from flask.json import jsonify
+import requests
+from flask import Flask, Response, g, request
+from http import HTTPStatus
+import random
 import logging
 
 app = Flask(__name__)
-
 if __name__ != "__main__":
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
 
-@app.route('/health')
+@app.route('/health', methods=["GET"])
 def check_health():
-    return "OK"
+    return "ok"
+
+
+@app.route('/map_interface', methods=["POST"])
+def map_interface():
+    return "ok"

9c3ec306f35d28e47ce44caaae6aec56184d4898
Me <maldonadojsm@gmail.com>
2021-08-31 20:36:36 +0200

    ??

--- mercator/app.py
+++ mercator/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
 
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
-hehe = "hoho"
 @app.route('/health')
 def check_health():
     return "OK"
 
 
-if __name__ == '__main__':
-    app.run()

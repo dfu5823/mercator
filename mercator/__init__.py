024576e92228b0997be08b3c06371b0ce3903f1c
Me <maldonadojsm@gmail.com>
2021-09-02 14:34:36 +0200

    fixing abs path circleci

--- mercator/__init__.py
+++ mercator/__init__.py
@@ -15,7 +15,7 @@ import cv2 as cv
 
 def create_app():
 
-    UPLOAD_FOLDER = '../Mercator/mercator/logs/'
+    UPLOAD_FOLDER = os.path.abspath('../Mercator/mercator/logs/')
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
     app = Flask(__name__)
